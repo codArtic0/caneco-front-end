59b2340c9eb053b201ffd07449e3901e0060c2a4
codArtic0 <raulsousadesigner@gmail.com>
2026-03-06 15:14:04 -0300

    Add: Mapeamento de dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,40 @@
+import "../styles/Dashboard.css";
+import {useNavigate} from 'react-router-dom';
+
 function Dashboard() {
+    const navigate = useNavigate();
+
+    const historico = () => {
+        navigate('/dashboard/historico');
+    };
+
+    const adicionarSaldo = () => {
+        navigate('/dashboard/adicionar-saldo');
+    };
+
+    const fecharRegistro = () => {
+        navigate('/dashboard/fechar-registro');
+    };
+
+    const novaVenda = () => {
+        navigate('/dashboard/nova-venda');
+    }
+
     return (
         <div>
-            <button>Histórico de vendas</button>
-            <button>Adicionar saldo</button>
-            <button>Fechar registro de caixa</button>
-            <button>Nova venda</button>
+            <div className="dashboard-container">
+                <div className="dashboard-header">
+                    <h1>DASHBOARD</h1>
+                </div>
+                <div className="dashboard-content">
+                    <button className="botao-azul" onClick={historico}>
+                        Histórico de vendas
+                    </button>
+                    <button className="botao-azul" onClick={adicionarSaldo}>Adicionar saldo</button>
+                    <button className="botao-azul" onClick={fecharRegistro}>Fechar registro de caixa</button>
+                    <button className="botao-azul" onClick={novaVenda}>Nova venda</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -9,6 +9,10 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+                <Route path="/dashboard/historico" element={<ProtectedRoute element={<h1>Histórico de vendas</h1>} />} />
+                <Route path="/dashboard/adicionar-saldo" element={<ProtectedRoute element={<h1>Adicionar saldo</h1>} />} />
+                <Route path="/dashboard/fechar-registro" element={<ProtectedRoute element={<h1>Fechar registro de caixa</h1>} />} />
+                <Route path="/dashboard/nova-venda" element={<ProtectedRoute element={<h1>Nova venda</h1>} />} />
             </Routes>
         </Router>
     );
