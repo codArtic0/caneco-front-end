2510fe396d4473c938f2870f07b645bd81b3696d
gabrielcaio11 <gabrielcaio848@gmail.com>
2026-03-13 16:35:43 -0300

    Add: Implement Alcoolicas and Refrigerantes pages with navigation from Bebidas

--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -4,9 +4,31 @@ import {useNavigate} from 'react-router-dom';
 
 function Bebidas(){
 
+    const navigate = useNavigate();
+
+    const alcoolicas = () => {
+        navigate('/dashboard/nova-venda/bebidas/alcoolicas');
+    }
+
+    const refrigerantes = () => {
+        navigate('/dashboard/nova-venda/bebidas/refrigerantes');
+    }
+
     return (
         <div>
-            <p> exemplo</p>
+            <div className="bebidas-container">
+                <div className="bebidas-header">
+                    <h1>BEBIDAS</h1>
+                </div>
+                <div className="bebidas-content">
+                    <button className="botao-azul" onClick={refrigerantes}>
+                        Refrigerantes
+                    </button>
+                    <button className="botao-azul" onClick={alcoolicas}>
+                        Alcoolicas
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,6 +6,8 @@ import ProtectedRoute from '../components/ProtectedRoute';
 import Historico from '../pages/Historico';
 import NovaVenda from '../pages/NovaVenda';
 import Bebidas from '../pages/Bebidas';
+import Alcoolicas from '../pages/Alcoolicas';
+import Refrigerantes from '../pages/Refrigerantes';
 
 function AppRoutes() {
     return (
@@ -18,6 +20,9 @@ function AppRoutes() {
                 <Route path="/dashboard/fechar-registro" element={<ProtectedRoute element={<h1>Fechar registro de caixa</h1>} />} />
                 <Route path="/dashboard/nova-venda" element={<ProtectedRoute element={<NovaVenda />} />} />
                 <Route path="/dashboard/nova-venda/bebidas" element={<ProtectedRoute element={<Bebidas />} />} />
+                <Route path="/dashboard/nova-venda/bebidas/alcoolicas" element={<ProtectedRoute element={<Alcoolicas />} />} />
+                <Route path="/dashboard/nova-venda/bebidas/refrigerantes" element={<ProtectedRoute element={<Refrigerantes />} />} />
+
             </Routes>
         </Router>
     );
